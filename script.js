5c07dfcdd84bf1eea994d85db85569b92b8bce4a
Sebastian <sebastianLevi84@gmail.com>
2025-01-02 12:06:12 +0100

    First steps (part 2)

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-/* container HEADER */
-const head = document.querySelector('header');
-/* content for the HEADER */
-let fTitle = document.createElement('h1');
-// let contLinks = document.createElement('div');
+// Head: (function declaration)
+function headComponents() {
+    const head = document.querySelector('header');
+    const fTitle = document.createElement('h1');
+
+    fTitle.innerHTML = "DysJS";
+    fTitle.classList.add('h1');
+    head.append(fTitle);    
+    head.classList.add('header');
+};
+
+// Section: (function expression)
+const sectComp = () => {
+    const sect = document.querySelector('section');
+    const contDivSect = document.createElement('div');
+
+    contDivSect.innerHTML = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`
+    sect.append(contDivSect);
+    sect.classList.add('section');
+
+    console.log(sect, contDivSect);   
+};
+
+headComponents();
+sectComp();
 
 
-/* settings: */
-fTitle.innerHTML = "DysJS";
-fTitle.classList.add('h1');
-head.append(fTitle);
-head.style.padding = '15px';
-head.style.backgroundColor = 'black';
-head.style.maxHeight = '70px';
-head.style.textAlign = 'center';
-/* functions: */
 
-console.log(head);
